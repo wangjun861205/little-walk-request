051ae870f874859223b599df443948d943a3ba32
wangjun861205 <444055828@qq.com>
2023-11-29 18:59:14 +0800

    savepoint

--- src/core/service.rs
+++ src/core/service.rs
@@ -25,10 +25,10 @@ where
 
     pub async fn create_walk_request(&self, request: WalkRequestCreate) -> Result<String, Error> {
         if request.should_start_after >= request.should_end_before {
-            return Err(Error::msg("非法的开始时间范围"));
+            return Err(Error::msg("开始时间范围起点不得大于等于终点"));
         }
         if request.should_end_after >= request.should_end_before {
-            return Err(Error::msg("非法的结束时间范围"));
+            return Err(Error::msg("结束时间范围起点不得大于等于终点"));
         }
         if request.should_start_after >= request.should_end_before {
             return Err(Error::msg("结束时间不得早于开始时间"));
